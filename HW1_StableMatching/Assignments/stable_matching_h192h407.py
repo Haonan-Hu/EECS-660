d6a5685bab113b51aa5b9fee32ae3a9b0020f397
Haonan Hu <hjgyhhn6328@gmail.com>
2021-05-01 13:05:34 -0500

    Done Assignment 6 and added 7

--- HW1_StableMatching/Assignments/stable_matching_h192h407.py
+++ HW1_StableMatching/Assignments/stable_matching_h192h407.py
@@ -1,6 +1,4 @@
 import sys
-# maybe use for auto run test
-# file_list = [a for a in range(10, 110, 10)]
 # initialize some variable
 # men and women will be list of lists, maybe easy to use?
 n = 0
@@ -36,7 +34,6 @@ def check_prefer_m1_over_m(prefer_list, woman, man, man1):
             index_man1 = i
         if prefer_list[woman-1][i] == man:
             index_man = i
-    # print(f"{man}:{index_man}, {man1}:{index_man1}")
     if index_man1 < index_man:
         return True
     else:
@@ -61,7 +58,6 @@ def stable_matching():
             if free_men[m-1] is True and not all(a == -1 for a in men[m-1]):
                 break
             m += 1
-
         # let w to be the highest ranked women in m's list to whom not yet proposed 
         for i in men[m-1]:
             # the woman i in m's list has not been proposed
@@ -73,14 +69,13 @@ def stable_matching():
             matched.append(tuple((m, w)))  # w and m engaged
             free_men[m-1] = False  # the man m become not free
             matched_women[w-1] = m  # the woman w engaged with m
-            men[m-1] = [-1 if x == w else x for x in men[m-1]]  # mark the woman in m's list as proposed
         # if woman w is already engaged with m1
         else:
             m1 = matched_women[w-1]  # the man woman currently matched
             # if m1 is prefered than m for woman w
             if check_prefer_m1_over_m(women, w, m, m1):
-                # mark off the woman that just proposed and man m remain free
-                men[m-1] = [-1 if x == w else x for x in men[m-1]]
+                # man m remain free
+                pass
             else:
                 # delete the old couple (m1, w)
                 matched.remove((m1, w))
@@ -89,6 +84,9 @@ def stable_matching():
                 free_men[m1-1] = True
                 free_men[m-1] = False
                 matched_women[w-1] = m
+        # mark the woman in m's list as proposed
+        men[m-1] = [-1 if x == w else x for x in men[m-1]]
+        # Once all man are not free break the loop
         if all(a is False for a in free_men):
             break
     matched = sorted(matched, key=lambda tup: tup[0])
